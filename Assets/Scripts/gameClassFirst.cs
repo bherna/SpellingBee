818780087367e43a7cd675497c8f1403eeda2ea1
bherna153@gmail.com <bherna153@gmail.com>
2022-07-27 17:26:14 -0600

    created first-word class first-game class

---
+++ Assets/Scripts/gameClassFirst.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameClassFirst : MonoBehaviour
+{
+
+
+
+    void Start()
+    {
+        //Get a list of words and letters
+
+
+        //create an empty word
+        wordClassFirst word = new wordClassFirst();
+
+    }
+
+
+    public void CheckWord()
+    {
+        // check if the word is long enough
+
+        //check if the word includes the GOLD letter
+
+        //chcek if the word is an actual word (from the list)
+
+    }
+
+    
+}
