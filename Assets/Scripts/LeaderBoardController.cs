50cbf2da9682559dd28c6aaf57ccdb9f83457282
bherna153@gmail.com <bherna153@gmail.com>
2022-08-15 16:22:01 -0600

    leaderboard can now show top 4 scores from online in the game now

--- Assets/Scripts/LeaderBoardController.cs
+++ Assets/Scripts/LeaderBoardController.cs
@@ -9,6 +9,8 @@ public class LeaderBoardController : MonoBehaviour
 
     public TMP_InputField MemberID, PlayerScore;
     public int ID;
+    private int MaxScores = 4;
+    public TMP_Text[] Entries;
 
 
     void Start()
@@ -28,6 +30,44 @@ public class LeaderBoardController : MonoBehaviour
             else
             {
                 Debug.Log("failed to start sessions" + response.Error);
+            }
+        });
+    }
+
+    public void ShowScores()
+    {
+
+        LootLockerSDKManager.GetScoreList(ID, MaxScores, (response) =>
+        {
+            if (response.success)
+            {
+                ///get top scores from online
+                ///LootLockerGetByListOfMembersResponse
+                LootLockerLeaderboardMember[] scores = response.items;
+                
+                
+                for (int i = 0; i < scores.Length; i++)
+                {
+
+                    Entries[i].text = scores[i].rank + ".   " + scores[i].score;
+                    
+                }
+
+
+                ///if we have less scores online than we can show on the leaderboard
+                if(scores.Length < MaxScores)
+                {
+                    for(int i = scores.Length; i < MaxScores; i++)
+                    {
+                        Entries[i].text = (i + 1).ToString() + ".   none";
+                    }
+                }
+
+
+            }
+            else
+            {
+                Debug.Log("failed: " + response.Error);
             }
         });
     }
