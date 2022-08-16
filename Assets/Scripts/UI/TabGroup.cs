ab0bb02fd078f2ac802ea26dd18d34c6a61a91b9
bherna153@gmail.com <bherna153@gmail.com>
2022-08-15 18:58:18 -0600

    added sprite folder and sprite assets, there are now working tabs for the game

--- Assets/Scripts/UI/TabGroup.cs
+++ Assets/Scripts/UI/TabGroup.cs
@@ -1,18 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TabGroup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public List<TabButton> tabButtons;
+    public Sprite tabIdle;
+    public Sprite tabHover;
+    public Sprite tabActive;
+    public TabButton selectedTab;
+    public List<GameObject> objectsToSwap;
+
+    //tab group component that our buttons can subscribe to 
+    public void Subscribe(TabButton button)
     {
-        
+        if(tabButtons == null)
+        {
+            tabButtons = new List<TabButton>();
+        }
+
+        tabButtons.Add(button);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    //button methods
+    //changes the state of our tabs when ever one of the button tabs is interacted with
+
+    public void OnTabEnter(TabButton button)
     {
+        ResetTabs();
+        if(selectedTab == null || button != selectedTab)
+        {
+            button.backGround.sprite = tabHover;
+        }
         
+
+    }
+
+    public void OnTabExit(TabButton button)
+    {
+        ResetTabs();
+
+    }
+
+    public void OnTabSelected(TabButton button)
+    {
+        selectedTab = button;
+        ResetTabs();
+        button.backGround.sprite = tabActive;
+
+        int index = button.transform.GetSiblingIndex();
+        for(int i = 0; i < objectsToSwap.Count; i++)
+        {
+            if (i == index)
+            {
+                objectsToSwap[i].SetActive(true);
+            }
+            else
+            {
+                objectsToSwap[i].SetActive(false);
+            }
+        }
+
+    }
+
+    public void ResetTabs()
+    {
+        foreach(TabButton button in tabButtons)
+        {
+            if(selectedTab != null && button == selectedTab) { continue; }
+            button.backGround.sprite = tabIdle;
+        }
     }
 }
